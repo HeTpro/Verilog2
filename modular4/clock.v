8b7bc83168c18561b893d34a1946d3d60f1661a5
esmarr58 <ruben.estrada@hetpro.com.mx>
2015-08-05 21:23:37 -0500

    Update clock.v

--- modular4/clock.v
+++ modular4/clock.v
@@ -1,31 +1,27 @@
 module clk 
 		#( 
-			parameter [8:0] th = 5,
-			parameter [8:0] tl = 5
-				
+			parameter [31:0] limite = 5
+					
 			) 
 			
 			(fin, fout);
 	input fin;
-	output reg fout=0;
+	output reg fout=1;
 	reg[31:0] cuenta1=0;
-	parameter[31:0] limite1=25000000;
+
 	
 	always @(posedge fin)
 		begin
 			
-			if(cuenta1>=th-1 )	
+			if (cuenta1>=limite-1)
 				begin
-					fout = 0;
-				end
-			else if (cuenta1>=(th+tl)-1)
-				begin
-					fout = 1;
+					fout = ~fout;
 					cuenta1=0;
 				end
 			else
 				begin
 				 cuenta1=cuenta1+1;
+				
 				end
 				
 		end
